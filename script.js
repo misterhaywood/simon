e7061de7494ddf04c42025c8c8abc989686b9d49
mrhaywood <mr.haywood@gmail.com>
2021-01-22 16:10:30 -0600

    friday refactoring

--- script.js
+++ script.js
@@ -2,23 +2,13 @@
 let computerMoves = [];
 let playerMoves = [];
 let player = 'blue'
+let padToFlash;
 let turn = document.getElementsByTagName('h2')[0];
 
-let allCells = document.querySelectorAll('.rbyg')
+let allPads = document.querySelectorAll('.pad')
 
-allCells.forEach(e => e.addEventListener('mousedown', e => e.classList.toggle('clicked')))
-allCells.forEach(e => e.addEventListener('mouseup', e => e.classList.toggle('clicked')))
-
-let count = 0;
-
-let button = document.getElementsByTagName('button')[0]
-button.addEventListener('click', reset)
-
-for (let i = 0; i < allCells.length; i++) {
-    allCells[i].addEventListener('click', runGame) //, {once: true})
-}
-
-reset()
+allPads.forEach(e => e.addEventListener('mousedown', e => e.target.classList.toggle('clicked')))
+allPads.forEach(e => e.addEventListener('mouseup', e => e.target.classList.toggle('clicked')))
 
 function getRandomColor() {
     let buttons = ['green', 'red', 'yellow', 'blue']
@@ -26,62 +16,43 @@ function getRandomColor() {
     return buttons[randomNumber];
 }
 
+let count = 0;
+
 let moves = [];
-for (let i = 0; i < 10; i++) {
+function getMoves(){
+  for (let i = 0; i < 10; i++) {
     moves.push(getRandomColor());
-    console.log(moves)
+  }
 }
 
-function runGame(event) {
-    count++;
-    event.preventDefault();
-
-    // computers' turn
-    if (player == 'blue') {
-        computerMoves.push(this.id)
-        changeOnClick(this)
-        // console.log(`computerMoves: ${computerMoves}`)
-
-        player = 'red'
-        turn.innerText = "My Turn"
-    }
-    // players' turn
-    else if (player == 'red') {
-        for (let i = 0; i < computerMoves.length; i++) {
-            playerMoves.push(this.id)
-            // console.log(`playerMoves: ${playerMoves}`)
-
-        }
-
-
-        player = 'blue'
-        turn.innerText = "Your Turn"
-        playerMoves = [];
-    }
-
-    // console.log(computerMoves, playerMoves);
-    // checkWinner()
+function removeClass(){
+  let aVariableWhatever = setTimeout(function(){
+    padToFlash.classList.remove('clicked')
+  }, 1000)
+  return aVariableWhatever;
 }
 
-// function checkWinner(){
-//     if (computerMoves.length > 0){
-//         if (computerMoves == playerMoves) {
-//             console.log(computerMoves, playerMoves);
-//         }
-//     }
-// }
+let theTimeout;
+function highlightPad (){
+  for (let k = 0; k < moves.length; k++) {
+    padToFlash = document.querySelector(`#${moves[k]}`)
+    console.log(padToFlash)
 
-function reset(){
-    count = 0;
-    computerMoves = [];
-    player = 'blue'
-    turn.innerText = "My Turn"
-
-    for (let i = 0; i < allCells.length; i++) {
-        // console.log(allCells[i].classList)
-        // allCells[i].classList.remove('red')
-        // allCells[i].classList.remove('blue')
-        allCells[i].addEventListener('click', runGame, {once: true})
-    }
-
+    padToFlash.classList.add('clicked')
+    console.log(padToFlash)
+    removeClass()
+  }
+  clearInterval(theTimeout)
 }
+
+function playSequence(e) {
+  e.preventDefault()
+
+  getMoves();
+  highlightPad();
+  console.log(moves)
+  moves = [];
+}
+
+let startButton = document.querySelector('button');
+startButton.addEventListener('click', playSequence)
